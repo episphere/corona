80b5543633de1318c9f6c1a007c4654afb2cf1ee
Jonas Almeida <jalmeida@mathbiol.org>
2020-03-12 12:36:18 -0400

    live links

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 [![Join the chat at https://gitter.im/episphere/corona](https://badges.gitter.im/episphere/corona.svg)](https://gitter.im/episphere/corona?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge&utm_content=badge)
 
-Exploring realtime interoperability with COVID-19 data
+Exploring realtime interoperability with COVID-19 data. The use of corona.js library is illustrated by a web page at https://episphere.github.io/episphere/corona, and an observable notebook at https://observablehq.com/@episphere/corona.
--- corona.js
+++ corona.js
@@ -8,6 +8,7 @@ corona.ui=(div=document.getElementById('coronaDiv'))=>{
     if(typeof(div)!='object'){
         warning('div not found')
     }else{
+        div.innerHTML='cororaDiv here ...'
         corona.div=div
     }
 }
@@ -15,3 +16,11 @@ corona.ui=(div=document.getElementById('coronaDiv'))=>{
 if(typeof(define)!='undefined'){
     define(corona)
 }
+
+corona.getDaily=async(day)=>{
+    let url=`https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/03-03-2020.csv`
+    return fetch('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/03-03-2020.csv').then(response => response.text()).then(data => console.log(data));
+}
+
+
+//https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/03-03-2020.csv
